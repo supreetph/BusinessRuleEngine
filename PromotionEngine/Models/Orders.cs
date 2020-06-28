864e35a8d84a0f73a414910fd0b66e4eb2741a60
Supreet <suprith.prakash.sp@gmail.com>
2020-06-28 15:34:00 +0530

    Add project files.

---
+++ PromotionEngine/Models/Orders.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine.Models
+{
+   public class Orders
+    {
+        public int orderId { get; set; }
+        public string OrderType { get; set; }
+    }
+}
